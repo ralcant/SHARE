98490a7bbea5390c4c023e99a94ccc84aceb7c32
tc27182 <55326476+tc27182@users.noreply.github.com>
2021-04-24 14:42:01 -0400

    integrated facebook-scraper, added functionality to get posts from other pages and provide a link to comment manually

--- commandline.py
+++ commandline.py
@@ -10,6 +10,8 @@ from generateConfessions import generateCommentGPT2
 from generateConfessions import postRandomConfessions
 from meme_utils import MemeGenerator
 import os
+import pyperclip
+from facebook_scraper import get_posts
 import shutil
 console = Console()
 import json
@@ -57,7 +59,7 @@ def choosePost(posts):
     index = options("[bold]Pick a post:[/bold]", choices)
     return index
     
-def generateComment(graph, post):
+def generateComment(graph, post, link=''):
     comments = generateCommentGPT2(post['message'], num=5)
     for i in range(len(comments)):
         print(f"COMMENT {i+1}: [#03c6fc]{comments[i]}[/#03c6fc]\n----")
@@ -67,12 +69,24 @@ def generateComment(graph, post):
     comment_link = None
     for i in range(len(comments)):
         if ans ==str(i+1)+"":
-            print(f"Posted comment")
+            print(f"Posting comment...")
             comment_link = makeComment(graph, post, comments[i])
             done = True 
     if not done:
         print("Okay, will not comment.")
     return comment_link
+def getPostsWrapper(index, graph=None, login=None):
+    if index == 0:
+        return convert(getPosts(graph, login['pageId']))['data']
+    else: #index=1, for now just beaverconfessions
+        posts = []
+        for post in get_posts('beaverconfessions',pages=5):
+            new_post = {}
+            new_post['message'] = convert(post['post_text'])
+            new_post['link'] = post['post_url']
+            new_post['id'] = post['post_id']
+            posts.append(new_post)
+        return posts
 def main():
     os.system("clear")
     console.rule("[bold blue]Welcome to SHARE, the MIT Confessions Bot")
@@ -81,14 +95,16 @@ def main():
     login = promptAccounts()
     graph = setAccount(login)
     memer = MemeGenerator("mit_meme_creator", "mit_meme_password", graph)
-    posts = convert(getPosts(graph, login['pageId']))['data']
+    page_index = options('Which page?', ['Fake MIT Confessions', 'beaverconfessions'])
+    posts = getPostsWrapper(page_index, graph, login)
     while True:
         index = choosePost(posts)
         if index == 1:
             break
         if index == 0:
             postRandomConfessions(graph)
-            posts = convert(getPosts(graph, login['pageId']))['data']
+#            posts = convert(getPosts(graph, login['pageId']))['data']
+            posts = getPostsWrapper(page_index)
             continue
         post = posts[index-2]
         print(f"CONFESSION: [#f5a6ff]{post['message']}[/#f5a6ff]\n----)")
@@ -99,7 +115,8 @@ def main():
             print("[#03c6fc]Type Comment:[/#03c6fc]")
             comment = input() 
             comment_link = makeComment(graph, post, comment)
-            print(f"[bold]Posted. See it here {comment_link} [/bold]")
+            if comment_link != None:
+                print(f"[bold]Posted. See it here {comment_link} [/bold]")
         else:
             memes = memer.get_all_memes()
             names = [meme['name'] for meme in memes]
